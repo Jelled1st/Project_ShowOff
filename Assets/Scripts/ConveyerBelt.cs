a3107671121a2bfd1721bbe6d70b2f07f7ef0e14
Jelle Vrieze <jelle.vrieze@gmail.com>
2020-05-05 20:44:19 +0200

    [ADD] ConveyerBelt, ConveyerBeltPusherBlock Script, DoTween and my test scene. DoTween is ignored by gitignore

---
+++ Assets/Scripts/ConveyerBelt.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyerBelt : MonoBehaviour
+{
+    [SerializeField] private GameObject _conveyerBelt;
+    [SerializeField] private GameObject _pusherBlockPrefab;
+    [SerializeField] private GameObject _wayPoints;
+    [SerializeField] private uint _sampleSize = 5;
+    [Tooltip("Speed is how many second it takes to travel 1 unit")][SerializeField] private float _speed = 2f;
+
+    private ConveyerPusherBlock[] _pusherBlocks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pusherBlocks = new ConveyerPusherBlock[_sampleSize];
+        //calculate the emptyspace up until the 
+        Vector3 emptySpace = GetSpaceBetweenWayPoints(0, _wayPoints.transform.childCount - 1) / (_sampleSize);
+        Vector3 spawnPosition = _wayPoints.transform.GetChild(0).transform.localPosition;
+        for(int i = 0; i < _sampleSize; ++i)
+        {
+            GameObject pusherBlock = Instantiate(_pusherBlockPrefab);
+            _pusherBlocks[i] = pusherBlock.GetComponent<ConveyerPusherBlock>();
+            _pusherBlocks[i].Init("Pusher_Block_" + i, this, this.gameObject.transform, spawnPosition + (emptySpace * i));
+            float wayPointDistance = (_wayPoints.transform.GetChild(1).position - (spawnPosition + (emptySpace * i))).magnitude;
+            _pusherBlocks[i].SetCurrentWayPoint(_wayPoints.transform.GetChild(1).gameObject, 1, _speed * wayPointDistance);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < _sampleSize; ++i)
+        {
+            if (_pusherBlocks[i] == null) continue;
+            if(_pusherBlocks[i].GetCurrentWayPoint() == null)
+            {
+                int wayPointIndex = _pusherBlocks[i].GetLastWayPointIndex();
+                if (wayPointIndex < _wayPoints.transform.childCount - 1)
+                {
+                    float wayPointDistance = GetSpaceBetweenWayPoints(wayPointIndex, ++wayPointIndex).magnitude;
+                    _pusherBlocks[i].SetCurrentWayPoint(_wayPoints.transform.GetChild(wayPointIndex).gameObject, wayPointIndex, _speed * wayPointDistance);
+                }
+                else
+                {
+                    Debug.Log("Reset " + i);
+                    // reset to start
+                    wayPointIndex = 1; // waypoint[0] is start point 
+                    float wayPointDistance = GetSpaceBetweenWayPoints(0, 1).magnitude;
+                    Transform wayPointTransform = _wayPoints.transform.GetChild(0);
+                    _pusherBlocks[i].transform.SetPositionAndRotation(wayPointTransform.localPosition, wayPointTransform.localRotation);
+                    _pusherBlocks[i].SetCurrentWayPoint(_wayPoints.transform.GetChild(wayPointIndex).gameObject, wayPointIndex, _speed * wayPointDistance);
+                }
+            }
+        }
+    }
+
+    private Vector3 GetSpaceBetweenWayPoints(int waypoint1, int waypoint2)
+    {
+        Vector3 distance = _wayPoints.transform.GetChild(waypoint2).transform.localPosition - _wayPoints.transform.GetChild(waypoint1).transform.localPosition;
+        return distance;
+    }
+}
---
+++ Assets/Scripts/ConveyerPusherBlock.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ConveyerPusherBlock : MonoBehaviour
+{
+    private static readonly Vector3 InitScale = new Vector3(0.10f, 0.3f, 0.75f);
+
+    private ConveyerBelt _conveyerBelt;
+    private GameObject _lastWayPoint;
+    private GameObject _currentWayPoint;
+    private int _lastWayPointIndex;
+    private int _currentWayPointIndex;
+
+    private bool _initCalled = false;
+
+    public void Init(string name, ConveyerBelt conveyerBelt, Transform parent, Vector3 position)
+    {
+        _conveyerBelt = conveyerBelt;
+
+        this.gameObject.name = name;
+        this.transform.SetParent(parent);
+        BoxCollider collider;
+        if (!this.gameObject.TryGetComponent<BoxCollider>(out collider))
+        {
+            collider = this.gameObject.AddComponent<BoxCollider>();
+        }
+
+        this.gameObject.transform.localScale = InitScale;
+        this.gameObject.transform.localPosition = position;
+
+        GameObject visualizer = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        visualizer.transform.SetParent(this.transform);
+        visualizer.transform.localPosition = new Vector3(0, 0, 0);
+        visualizer.transform.localScale = new Vector3(1, 1, 1);
+
+        _initCalled = true;
+    }
+
+    void Start()
+    {
+        if (!_initCalled) Debug.LogError("ConveyerPusherBlock not initialized, call Init() before Start()");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if there is no waypoint to move to, return
+        if (_currentWayPoint == null) return;
+
+        if (this.transform.position == _currentWayPoint.transform.position)
+        {
+            SetCurrentWayPoint(null, -1, 0);
+        }
+    }
+
+    #region waypoint getters and setters
+
+    public GameObject GetLastWayPoint()
+    {
+        return _lastWayPoint;
+    }
+
+    public int GetLastWayPointIndex()
+    {
+        return _lastWayPointIndex;
+    }
+
+    public GameObject GetCurrentWayPoint()
+    {
+        return _currentWayPoint;
+    }
+
+    public int GetCurrentWayPointIndex()
+    {
+        return _currentWayPointIndex;
+    }
+
+    public void SetCurrentWayPoint(GameObject wayPoint, int index, float time)
+    {
+        _lastWayPoint = _currentWayPoint;
+        _lastWayPointIndex = _currentWayPointIndex;
+
+        _currentWayPoint = wayPoint;
+        _currentWayPointIndex = index;
+
+        if(_currentWayPoint != null)
+        {
+            this.transform.DOLocalMove(wayPoint.transform.position, time);
+            this.transform.DOLocalRotate(wayPoint.transform.rotation.eulerAngles, time);
+        }
+    }
+
+    #endregion
+}
