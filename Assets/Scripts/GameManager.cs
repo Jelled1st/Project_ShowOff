217230d58bac6a1514a02e4f72941ca97d54a9c4
Gabe <glebzex@mail.ua>
2020-05-05 16:34:30 +0200

    [ADD] Game Manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _packagePrefab;
+
+    [SerializeField] private Transform _startPosition;
+
+    [SerializeField] private float _groundLevel;
+
+    [SerializeField] private float _respawnTime;
+
+    [SerializeField] private float _pushForce;
+
+
+    private GameObject _package;
+
+    private void Start()
+    {
+        _package = Instantiate(_packagePrefab, _startPosition);
+        _package.GetComponentInChildren<Rigidbody>().AddForce(Vector3.left * _pushForce);
+    }
+
+    private void Update()
+    {
+        if (_package.transform.position.y <= _groundLevel)
+        {
+            Invoke(nameof(RespawnPackage), _respawnTime);
+        }
+    }
+
+    private void RespawnPackage()
+    {
+        _package.transform.position = _startPosition.position;
+        _package.transform.rotation = Quaternion.identity;
+    }
+}
