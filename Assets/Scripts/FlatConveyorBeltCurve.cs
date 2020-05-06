119237336be1422c0a1924878b8fda9c6dfb839c
Jelle Vrieze <jelle.vrieze@gmail.com>
2020-05-06 20:50:06 +0200

    [ADD] FlatConveyerBeltCurve script and prefab

--- Assets/Scripts/FlatConveyorBeltCurve.cs
+++ Assets/Scripts/FlatConveyorBeltCurve.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlatConveyorBelt : MonoBehaviour
+public class FlatConveyorBeltCurve : MonoBehaviour
 {
     [SerializeField] private float _speed = 1;
     [SerializeField] private Material _conveyorMaterial;
 
     private Rigidbody _rBody;
+    //magic number so that the speed of the moving package matches the speed of the moving texture - eye candy
+    private float _eyeCandySpeedMultiplier = 0.7f;
+
     // Start is called before the first frame update
     void Start()
     {
-        if(!TryGetComponent<Rigidbody>(out _rBody))
+        if (!TryGetComponent<Rigidbody>(out _rBody))
         {
             _rBody = this.gameObject.AddComponent<Rigidbody>();
         }
@@ -23,8 +26,8 @@ public class FlatConveyorBelt : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 pos = _rBody.position;
-        _rBody.position += _rBody.transform.right * -_speed * Time.deltaTime;
-        _rBody.MovePosition(pos);
+        Quaternion rot = _rBody.rotation;
+        _rBody.rotation *= Quaternion.Euler(0, _speed*_eyeCandySpeedMultiplier, 0);
+        _rBody.MoveRotation(rot);
     }
 }
