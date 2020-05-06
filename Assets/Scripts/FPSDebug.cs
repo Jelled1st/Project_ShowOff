c2480d7014d104ffc368acbc59dcff42643a927d
Jelle Vrieze <jelle.vrieze@gmail.com>
2020-05-06 14:23:22 +0200

    [ADD] FrameTime debug graph to FPSDebug

--- Assets/Scripts/FPSDebug.cs
+++ Assets/Scripts/FPSDebug.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class FPSDebug : MonoBehaviour
 {
 	float deltaTime = 0.0f;
+	int[] frameTimes;
+
+	void Start()
+	{
+		frameTimes = new int[100];
+	}
 
 	void Update()
 	{
 		//deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
 		deltaTime = Time.unscaledDeltaTime;
+		AddFrameTime(Mathf.RoundToInt(deltaTime * 1000));
+	}
+
+	private void AddFrameTime(int time)
+	{
+		for(int i = 0; i < frameTimes.Length-1; ++i)
+		{
+			frameTimes[i] = frameTimes[i + 1];
+		}
+		frameTimes[frameTimes.Length - 1] = time;
 	}
 
 	void OnGUI()
@@ -26,5 +42,24 @@ public class FPSDebug : MonoBehaviour
 		float fps = 1.0f / deltaTime;
 		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
 		GUI.Label(rect, text, style);
+
+
+		int width = 2;
+		Rect debuggerRect = new Rect(0, h-100, width * frameTimes.Length, 100);
+		Texture2D frameTimeRect = new Texture2D((int)debuggerRect.width, (int)debuggerRect.height);
+
+		for (int i = 0; i < frameTimes.Length; ++i)
+		{
+			for(int y = 0; y < frameTimes[i]; ++y)
+			{
+				for (int x = 0; x < width; ++x)
+				{
+					frameTimeRect.SetPixel((width*i)+x, y, new Color((frameTimes[i]/125), (255-frameTimes[i]/255), 0));
+				}
+			}
+		}
+		frameTimeRect.Apply();
+		GUI.skin.box.normal.background = frameTimeRect;
+		GUI.Box(debuggerRect, GUIContent.none);
 	}
 }
