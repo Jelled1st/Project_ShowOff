1720557231dd416e5d11c08cc2df2f00d90b8b78
Gabe <glebzex@mail.ua>
2020-05-06 12:58:24 +0200

    [ADD] FinishTriggerHit event

---
+++ Assets/FinishTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class FinishTrigger : MonoBehaviour
+{
+    [Tooltip("Invoked when a rigidbody entered the attached trigger")] [SerializeField]
+    private FinishTriggerHit _finishTriggerHit;
+
+    private Collider _finishTrigger;
+    public FinishTriggerHit FinishTriggerHit => _finishTriggerHit;
+
+    private void Awake()
+    {
+        _finishTriggerHit = new FinishTriggerHit();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        FinishTriggerHit.Invoke(other.gameObject);
+    }
+}
+
+[Serializable]
+public class FinishTriggerHit : UnityEvent<GameObject>
+{
+}
