9496995f2ab8b017587003a7010604c65c39fcfb
Gabe <glebzex@mail.ua>
2020-05-12 16:17:57 +0200

    [CHANGE] Applied state machine to the factory

---
+++ Assets/Scripts/Factory/FactoryStageSettings.cs
@@ -0,0 +1,13 @@
+using NaughtyAttributes;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FactoryStageSettings : StageSettings
+{
+    [field: SerializeField] public GameObject PackagePrefab { get; private set; }
+    [field: SerializeField] public float GroundLevel { get; private set; }
+    [field: SerializeField] public float RespawnTime { get; private set; }
+    [field: SerializeField] public float PushForce { get; private set; }
+    [field: SerializeField] public float StuckCheckTime { get; private set; }
+    [field: SerializeField] public float StuckDistance { get; private set; }
+}
---
+++ Assets/Scripts/Factory/PackageManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using StateMachine;
+using UnityEngine;
+
+namespace Factory
+{
+    public class PackageManager : LazySingleton<PackageManager>
+    {
+        private static FactoryStageSettings FactoryStageSettings =>
+            GameStageManager.GetStageSettings<FactoryStageSettings>();
+
+        private void OnEnable()
+        {
+            PackageController.PackageStuck += OnPackageStuck;
+            PackageController.PackageWentUnderGround += OnPackageWentUnderGround;
+        }
+
+        private void OnDisable()
+        {
+            PackageController.PackageStuck -= OnPackageStuck;
+            PackageController.PackageWentUnderGround -= OnPackageWentUnderGround;
+        }
+
+        private void OnPackageWentUnderGround(PackageController obj)
+        {
+            RespawnPackageDelayed(obj);
+        }
+
+        private void OnPackageStuck(PackageController obj)
+        {
+            RespawnPackageDelayed(obj);
+        }
+
+        private void RespawnPackageDelayed(PackageController packageController)
+        {
+            StartCoroutine(WaitAndExecute(FactoryStageSettings.RespawnTime,
+                () => { RespawnPackageImmediate(packageController); }));
+        }
+
+        private static IEnumerator WaitAndExecute(float delay, Action action)
+        {
+            yield return new WaitForSeconds(delay);
+            action();
+        }
+
+        private static void RespawnPackageImmediate(PackageController packageController)
+        {
+            Destroy(packageController.gameObject);
+            SpawnPackage();
+        }
+
+        public static void SpawnPackage()
+        {
+            var startPosition = GameObject.FindWithTag("StartPosition");
+
+            if (startPosition == null)
+            {
+                throw new Exception(
+                    "Can't find the start position! (Create and position an empty gameobject with tag 'StartPosition')");
+            }
+
+            var newPackage = Instantiate(FactoryStageSettings.PackagePrefab, startPosition.transform.position,
+                FactoryStageSettings.PackagePrefab.transform.rotation);
+
+            newPackage.GetComponentInChildren<Rigidbody>()
+                .AddForce(newPackage.transform.forward * FactoryStageSettings.PushForce);
+        }
+    }
+}
