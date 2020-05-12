7fff4776d277add2b4102fc5785529d51cf74783
Chloe <eggplantstudio6@gmail.com>
2020-05-12 15:58:43 +0200

    [ADD] Dumb shit don't look at it

--- Assets/Scenes/Chloe Tests/FrySpawner.cs
+++ Assets/Scenes/Chloe Tests/FrySpawner.cs
@@ -7,13 +7,33 @@ public class FrySpawner : MonoBehaviour
     public GameObject[] fries;
     public float rotationSpeed;
     public float force;
+    public int amount;
+    public float timeToLive;
+    private GameObject clone;
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(fries[Random.Range(0, fries.Length)], transform.position, transform.rotation)
-            .GetComponent<Rigidbody>().AddForce(transform.up * Random.Range(force - 200, force + 200));
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mouse = Input.mousePosition;
+            Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+            RaycastHit hit;
+            if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+            {
+                gameObject.transform.position = hit.point;
+            }
+                for (int i = 0; i < amount; i++)
+                {
+                Destroy(Instantiate(fries[Random.Range(0, fries.Length)], transform.position, transform.rotation), timeToLive);
+                        
+                    transform.Rotate(new Vector3(0, rotationSpeed, 0), Space.World);
+                for (int a = 0; a < fries.Length; a++)
+                {
+                    fries[a].GetComponent<Rigidbody>().AddForce(transform.up * Random.Range(force - 200, force + 200));
+                }
+            }
 
-        transform.Rotate(new Vector3(0, rotationSpeed, 0), Space.World);
+        }
     }
 }
