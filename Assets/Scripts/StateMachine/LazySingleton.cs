67a2091d3edc72cc48eab8004f1153a2fcf4d2e8
Gabe <glebzex@mail.ua>
2020-05-13 22:26:10 +0200

    [FIX] Singleton again............

--- Assets/Scripts/StateMachine/LazySingleton.cs
+++ Assets/Scripts/StateMachine/LazySingleton.cs
@@ -18,11 +18,17 @@ namespace StateMachine
             }
         }
 
+        private void Awake()
+        {
+            InitializeSingleton();
+        }
+
         private static void InitializeSingleton()
         {
             if (_instance != null && !_instance.Equals(null)) return;
 
             var singletons = FindObjectsOfType<T>();
+            
             if (singletons.Length == 1)
             {
                 _instance = singletons.First();
@@ -30,7 +36,7 @@ namespace StateMachine
 
             if (singletons.Length > 1)
             {
-                throw new Exception($"More than one object of type {{{typeof(T)}}} is present in scene!");
+                Debug.LogError($"More than one object of type {{{typeof(T)}}} is present in scene!");
             }
 
             if (singletons.Length == 0)
