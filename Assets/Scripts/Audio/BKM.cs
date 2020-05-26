b1f3c0c6963d843e56ac671ce39f33e1da710d1e
Avery <averyfukawa@gmail.com>
2020-05-26 22:57:24 +0200

    [ADD] SFX playing script, BKM playing script

---
+++ Assets/Scripts/Audio/BKM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class BKM : MonoBehaviour
+{
+    //[SerializeField] private int currentMusic;
+    [FMODUnity.EventRef, SerializeField] private string background;
+    [SerializeField, Range(0.0f, 1.0f)] private float volumeTesting = 0.45f;
+    
+    private FMOD.Studio.EventInstance _music;
+
+    private void Start()
+    {
+        _music = FMODUnity.RuntimeManager.CreateInstance(background);
+        //_music.setParameterByName("song", currentMusic);
+        _music.setVolume(volumeTesting);
+        _music.start();
+    }
+}
---
+++ Assets/Scripts/Audio/SFX.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX : MonoBehaviour
+{
+    
+    [FMODUnity.EventRef] public string sfxEvent;
+    //public string sfxEvent;
+        
+    private bool _isPlaying = false;
+
+    [SerializeField] private float clipLength;
+
+    public void SoundDig()
+    {
+        Play("SFX_FARM/Dig");
+        
+        clipLength = 0.9f;
+        StartCoroutine(WaitForEnd(clipLength));
+    }
+
+    public void SoundWater()
+    {
+        Play("SFX_FARM/Water");
+        
+        clipLength = 2.9f;
+        StartCoroutine(WaitForEnd(clipLength));
+    }
+    
+    /* Wherever the sound needs to play
+     [In the script]
+     private GameObject _audioManager;
+     
+     Start() 
+     {
+        _audioManager = GameObject.FindGameObjectWithTag("AudioManager");
+     }
+     
+     [In the area where it needs to play]
+     _audioManager.GetComponent<SFX>().SoundPlaceholder();     
+     */
+
+    #region playRules
+    public void Play(string fmodEvent)
+    {
+        Debug.Log("Audio is playing: " + fmodEvent);
+        if (_isPlaying == false)
+        {
+            sfxEvent = "event:/" + fmodEvent;
+                
+            FMODUnity.RuntimeManager.PlayOneShot(sfxEvent, transform.position);
+                
+            _isPlaying = true;
+        }
+    }
+
+    private IEnumerator WaitForEnd(float length)
+    {
+        if (length <= 0) throw new ArgumentOutOfRangeException("Length of clip is not set in " + " " + gameObject.name + nameof(length));
+            
+        length = clipLength;
+        yield return new WaitForSeconds(length);
+        _isPlaying = false;
+    }
+    #endregion
+}
