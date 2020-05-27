389670b9b5333b273328323da9e8969245f74713
Avery <averyfukawa@gmail.com>
2020-05-27 17:42:22 +0200

    [ADD] [CHANGE] Uprooting SFX, Spraying SFX, fixes to SFX Script, changes to BKM Script

--- Assets/Scripts/Audio/BKM.cs
+++ Assets/Scripts/Audio/BKM.cs
@@ -6,17 +6,59 @@ using UnityEngine;
 
 public class BKM : MonoBehaviour
 {
-    //[SerializeField] private int currentMusic;
-    [FMODUnity.EventRef, SerializeField] private string background;
-    [SerializeField, Range(0.0f, 1.0f)] private float volumeTesting = 0.45f;
-    
-    private FMOD.Studio.EventInstance _music;
+    [FMODUnity.EventRef, SerializeField] private string _musicFarm = "event:/BKM/Farm Music";
+    [FMODUnity.EventRef, SerializeField] private string _truckDrive = "event:/SFX_TRUCK/Truck Driving";
+    [FMODUnity.EventRef, SerializeField] private string _truckIdle = "event:/SFX_TRUCK/Truck Idle";
+    //[FMODUnity.EventRef] private string _musicFactory;
 
+    private FMOD.Studio.EventInstance _instanceSong;
+    private FMOD.Studio.EventInstance _instanceTransitionFx;
+    
     private void Start()
     {
-        _music = FMODUnity.RuntimeManager.CreateInstance(background);
-        //_music.setParameterByName("song", currentMusic);
-        _music.setVolume(volumeTesting);
-        _music.start();
+        FarmMusic();
+    }
+
+    /*
+     Debugging if functions work properly
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TruckDriving();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            TruckTransition();
+        }
+    }
+    */
+    
+    private void FarmMusic()
+    {
+        _instanceSong = FMODUnity.RuntimeManager.CreateInstance(_musicFarm);
+        _instanceSong.start();
+    }
+    public void TruckDriving()
+    {
+        _instanceTransitionFx = FMODUnity.RuntimeManager.CreateInstance(_truckDrive);
+        _instanceTransitionFx.start();
+        _instanceTransitionFx.release();
+        StartCoroutine(WaitForEnd(13.42f));
+    }
+
+    public void TruckTransition()
+    {
+        _instanceTransitionFx = FMODUnity.RuntimeManager.CreateInstance(_truckIdle);
+        _instanceTransitionFx.start();
+        _instanceTransitionFx.release();
+    }
+
+    private IEnumerator WaitForEnd(float length)
+    {
+        yield return new WaitForSeconds(length);
+        _instanceSong.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        _instanceSong.release();
     }
 }
--- Assets/Scripts/Audio/SFX.cs
+++ Assets/Scripts/Audio/SFX.cs
@@ -1,18 +1,40 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
 
 public class SFX : MonoBehaviour
 {
     
-    [FMODUnity.EventRef] public string sfxEvent;
+    [FMODUnity.EventRef, HideInInspector] public string sfxEvent;
     //public string sfxEvent;
         
     private bool _isPlaying = false;
 
     [SerializeField] private float clipLength;
 
+    /* Wherever the sound needs to play
+     [In the script]
+     private GameObject _audioManager;
+     private SFX _soundScript
+     
+     Start() 
+     {
+        _audioManager = GameObject.FindGameObjectWithTag("AudioManager");
+        _soundScript = _audioManager.GetComponent<SFX>().
+     }
+     
+     [In the area where it needs to play]
+     _soundScript.Sound[Name]();     
+    */
+    
+    // Digging Shovel Sound
+
+    private FMOD.Studio.EventInstance _swarmInstance;
+
+    #region FarmSounds
     public void SoundDig()
     {
         Play("SFX_FARM/Dig");
@@ -21,6 +43,7 @@ public class SFX : MonoBehaviour
         StartCoroutine(WaitForEnd(clipLength));
     }
 
+    // Watering can Sound
     public void SoundWater()
     {
         Play("SFX_FARM/Water");
@@ -29,19 +52,53 @@ public class SFX : MonoBehaviour
         StartCoroutine(WaitForEnd(clipLength));
     }
     
-    /* Wherever the sound needs to play
-     [In the script]
-     private GameObject _audioManager;
-     
-     Start() 
-     {
-        _audioManager = GameObject.FindGameObjectWithTag("AudioManager");
-     }
-     
-     [In the area where it needs to play]
-     _audioManager.GetComponent<SFX>().SoundPlaceholder();     
-     */
+    // Plant growing through stages Sound
+    public void SoundPlantGrowth()
+    {
+        Play("SFX_FARM/Grow");
+        
+        clipLength = 0.45f;
+        StartCoroutine(WaitForEnd(clipLength));
+    }
+    
+    // Sound Spraying vitamins
+    public void SoundPesticide()
+    {
+        Play("SFX_FARM/Pesticide");
+        
+        clipLength = 1.4f;
+        StartCoroutine(WaitForEnd(clipLength));
+    }
+    
+    // Sound before dragging into the truck
+    public void SoundUproot()
+    {
+        Play("SFX_FARM/Uproot");
+        
+        clipLength = 0.45f;
+        StartCoroutine(WaitForEnd(clipLength));
+    }
+    
+    #endregion
+    
+    // Sound Locust Swarm
+    public void SoundSwarm()
+    {
+        _swarmInstance = FMODUnity.RuntimeManager.CreateInstance("event:/SFX_FARM/Swarm");
+        _swarmInstance.start();
 
+        clipLength = 1.0f;
+        StartCoroutine(WaitForEnd(clipLength));
+    }
+
+    // Sound Locust Kill
+    public void SoundSwarmStop()
+    {
+        Debug.Log("Audio is playing: " + _swarmInstance);
+        _swarmInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        _swarmInstance.release();
+    }
+    
     #region playRules
     public void Play(string fmodEvent)
     {
