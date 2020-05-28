8a3c17c2551363908cd58aa7cfecf7180611f237
Gabe <glebzex@mail.ua>
2020-05-28 22:44:30 +0300

    [CHANGE] FrySpawner improved to ObjectSpawner

---
+++ Assets/Scenes/Chloe Tests/ObjectSpawner.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using NaughtyAttributes;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _objects;
+    [SerializeField] private float _spawnerRotationSpeed;
+
+    [MinMaxSlider(0, 2000)] [SerializeField]
+    private Vector2 _pushForce;
+
+    [SerializeField] private float _spawnInterval;
+
+
+    [SerializeField] private bool _spawnEndlessly;
+
+    [HideIf(nameof(_spawnEndlessly))] [SerializeField]
+    private int _objectCount;
+
+    [SerializeField] private bool _isDestroyedAfterTime;
+
+    [ShowIf(nameof(_isDestroyedAfterTime))] [SerializeField]
+    private float _timeBeforeDestroy;
+
+    private GameObject _clone;
+
+    private void Start()
+    {
+        if (_objects.Length == 0)
+            return;
+
+        if (Mathf.Abs(_spawnerRotationSpeed) < .01f)
+        {
+            DOTween.Sequence()
+                .AppendCallback(() => { transform.Rotate(new Vector3(0, _spawnerRotationSpeed, 0), Space.World); })
+                .SetLoops(-1);
+        }
+
+
+        if (_spawnEndlessly)
+        {
+            DOTween.Sequence().AppendCallback(Spawn).AppendInterval(_spawnInterval).SetLoops(-1);
+        }
+        else
+        {
+            DOTween.Sequence().AppendCallback(Spawn).AppendInterval(_spawnInterval).SetLoops(_objectCount);
+        }
+    }
+
+    private void Spawn()
+    {
+        var newObject = Instantiate(_objects[Random.Range(0, _objects.Length)], transform.position, transform.rotation);
+
+        newObject.TryGetComponent(out Rigidbody rBody);
+
+        if (!rBody.Equals(null))
+            rBody.AddForce(transform.up * Random.Range(_pushForce.x, _pushForce.y));
+
+        if (_isDestroyedAfterTime)
+            Destroy(newObject, _timeBeforeDestroy);
+    }
+}
