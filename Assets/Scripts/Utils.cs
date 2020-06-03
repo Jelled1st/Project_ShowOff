1cca9de61cb3974810a48c38e6ee2e0b379fa2e6
Gabe <glebzex@mail.ua>
2020-06-03 11:22:22 +0300

    [FIX] Revert condition

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -2,6 +2,6 @@ public static class Utils
 {
     public static T NullIfEqualsNull<T>(this T unityObject) where T : UnityEngine.Object
     {
-        return unityObject.Equals(null) ? unityObject : null;
+        return !unityObject.Equals(null) ? unityObject : null;
     }
 }
