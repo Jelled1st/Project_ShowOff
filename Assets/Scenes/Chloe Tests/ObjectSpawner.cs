42b1c507a064f590ec3d99c95b350641322a43ee
Gabe <glebzex@mail.ua>
2020-06-05 11:46:10 +0300

    [ADD] Object spawner can stop spawning now

--- Assets/Scenes/Chloe Tests/ObjectSpawner.cs
+++ Assets/Scenes/Chloe Tests/ObjectSpawner.cs
@@ -5,25 +5,33 @@ using Random = UnityEngine.Random;
 
 public class ObjectSpawner : MonoBehaviour
 {
-    [Header("Green axis (Y) is the throw direction")] [SerializeField]
+    [Header("Green axis (Y) is the throw direction")]
+    [SerializeField]
     private GameObject[] _objects;
 
 
-    [MinMaxSlider(0, 2000)] [SerializeField]
+    [MinMaxSlider(0, 2000)]
+    [SerializeField]
     private Vector2 _pushForce;
 
-    [SerializeField] private float _spawnInterval;
+    [SerializeField]
+    private float _spawnInterval;
 
-    [SerializeField] private bool _spawnEndlessly;
+    [SerializeField]
+    private bool _spawnEndlessly;
 
-    [HideIf(nameof(_spawnEndlessly))] [SerializeField]
+    [HideIf(nameof(_spawnEndlessly))]
+    [SerializeField]
     private int _objectCount;
 
-    [SerializeField] private bool _isDestroyedAfterTime;
+    [SerializeField]
+    private bool _isDestroyedAfterTime;
 
-    [ShowIf(nameof(_isDestroyedAfterTime))] [SerializeField]
+    [ShowIf(nameof(_isDestroyedAfterTime))]
+    [SerializeField]
     private float _timeBeforeDestroy;
 
+    private Tween _spawnTween;
     private GameObject _clone;
 
     private void Start()
@@ -33,12 +41,18 @@ public class ObjectSpawner : MonoBehaviour
 
         if (_spawnEndlessly)
         {
-            DOTween.Sequence().AppendCallback(Spawn).AppendInterval(_spawnInterval).SetLoops(-1);
+            _spawnTween = DOTween.Sequence().AppendCallback(Spawn).AppendInterval(_spawnInterval).SetLoops(-1);
         }
         else
         {
-            DOTween.Sequence().AppendCallback(Spawn).AppendInterval(_spawnInterval).SetLoops(_objectCount);
+            _spawnTween = DOTween.Sequence().AppendCallback(Spawn).AppendInterval(_spawnInterval)
+                .SetLoops(_objectCount);
         }
+    }
+
+    public void StopSpawning()
+    {
+        _spawnTween.Kill();
     }
 
     private void Spawn()
