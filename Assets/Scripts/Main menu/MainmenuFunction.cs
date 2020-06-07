085fe7a342ee3c34b77e3dd9a770297c33489b44
iVendrix <58091030+iVendrix@users.noreply.github.com>
2020-06-07 18:40:53 +0200

    [ADD] MainMenu Scene, MainmenuFunctions & Rotatesky script, UI elements

---
+++ Assets/Scripts/Main menu/MainmenuFunction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainmenuFunction : MonoBehaviour
+{
+
+    public AudioSource buttonPress;
+      
+
+    public void PlayGame()
+    {
+        buttonPress.Play();
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
