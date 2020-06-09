3f00b66adde2a579bf37b5e529904160f31a82f6
Gabe <glebzex@mail.ua>
2020-06-09 17:33:54 +0300

    [ADD] Factory tutorial controller

---
+++ Assets/Scripts/Factory/FactoryTutorialController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Factory;
+using NaughtyAttributes;
+using StateMachine;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+[Serializable]
+public class TutorialPart
+{
+    public CollisionCallback _trigger;
+
+    public GameObject _visuals;
+    // public Transform _cameraPosition;
+
+    [NonSerialized]
+    private bool _passed;
+
+    [NonSerialized]
+    private bool _awaitingPass;
+
+    public void Init()
+    {
+        _trigger.TriggerEnter += OnTriggerEnter;
+
+        _visuals.SetActive(false);
+    }
+
+    public void Cleanup()
+    {
+        _trigger.TriggerEnter -= OnTriggerEnter;
+
+        DOTween.Sequence()
+            // .Append(Camera.main.transform.DOMove(FactoryTutorialController.CameraInitialPosition, 2f))
+            .AppendCallback(() =>
+            {
+                Time.timeScale = 1f;
+                _visuals.SetActive(false);
+            }).SetUpdate(true);
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Time.timeScale = 0;
+        // Object.FindObjectsOfType<FlatConveyorBelt>().ToggleAll(false);
+
+        Vector3 initialScale = _visuals.transform.localScale;
+
+        DOTween.Sequence()
+            // .Append(Camera.main.transform.DOMove(_cameraPosition.position, 2f))
+            .AppendCallback(() => { _visuals.SetActive(true); })
+            .Append(_visuals.transform.DOPunchScale(initialScale * 2f, 0.5f).SetUpdate(true))
+            .AppendCallback(() => CoroutineHandler.StartUniqueCoroutine(WaitForPassed()))
+            // .AppendInterval(3f).AppendCallback(() =>
+            // {
+            //     Debug.Log("passed interval");
+            //     _passed = true;
+            // })
+            .SetUpdate(true);
+    }
+
+    private IEnumerator WaitForPassed()
+    {
+        _awaitingPass = true;
+        yield return new WaitUntil(() => _passed);
+        Cleanup();
+    }
+
+    public void Passed()
+    {
+        if (_awaitingPass)
+            _passed = true;
+    }
+}
+
+public class CoroutineHandler : LazySingleton<CoroutineHandler>
+{
+    public static void StartUniqueCoroutine(IEnumerator coroutine)
+    {
+        Instance.StartCoroutine(coroutine);
+    }
+}
+
+public class FactoryTutorialController : MonoBehaviour
+{
+    [BoxGroup("Slowdown")]
+    [SerializeField]
+    private TutorialPart _slowdownPart;
+
+    [BoxGroup("Slowdown")]
+    [SerializeField]
+    private FlatConveyorBelt _slowdownBelt;
+
+
+    [BoxGroup("Speedup")]
+    [SerializeField]
+    private TutorialPart _speedupPart;
+
+
+    [BoxGroup("Speedup")]
+    [SerializeField]
+    private FlatConveyorBelt _speedupBelt;
+
+
+    [BoxGroup("Rotation")]
+    [SerializeField]
+    private TutorialPart _rotationPart;
+
+
+    [BoxGroup("Rotation")]
+    [SerializeField]
+    private FlatConveyorBelt _rotationBelt;
+
+    [SerializeField]
+    private float _timeToHold = 1.5f;
+
+    private void OnEnable()
+    {
+        _slowdownPart.Init();
+        _speedupPart.Init();
+        _rotationPart.Init();
+
+        _slowdownBelt.BeltHeld += delegate(float f)
+        {
+            if (f > _timeToHold) _slowdownPart?.Passed();
+        };
+        _speedupBelt.BeltHeld += delegate(float f)
+        {
+            if (f > _timeToHold) _speedupPart?.Passed();
+        };
+        _rotationBelt.BeltPressed += delegate { _rotationPart?.Passed(); };
+    }
+}
