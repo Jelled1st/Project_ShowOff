091a96fd6bc872dab46f211a8098c18d87d52c4c
Avery <averyfukawa@gmail.com>
2020-06-12 13:19:47 +0200

    [FIX] Slightly beginning to fix the 2 music events at the same time bug

--- Assets/Scripts/Audio/BKM.cs
+++ Assets/Scripts/Audio/BKM.cs
@@ -3,23 +3,86 @@ using System.Collections;
 using System.Collections.Generic;
 using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 
 public class BKM : MonoBehaviour
 {
+    [Header("Menu")]
+    [FMODUnity.EventRef, SerializeField] private string musicMenu = "event:/BKM/Menu Music";
+    
+    [Space, Header("Farm")]
     [FMODUnity.EventRef, SerializeField] private string musicFarm = "event:/BKM/Farm Music";
     [FMODUnity.EventRef, SerializeField] private string farmTruckDrive = "event:/SFX_TRUCK/Farm Truck Driving";
-    [FMODUnity.EventRef, SerializeField] private string farmTruckTransit = "event:/SFX_TRUCK/Farm Truck Transit";
-    
+
+    [Space, Header("Factory")]
     [FMODUnity.EventRef, SerializeField] private string musicFactory = "event:/BKM/Factory Music";
     [FMODUnity.EventRef, SerializeField] private string factoryHum = "event:/SFX_FACTORY/Conveyor Hum";
+
+    [Space, Header("Kitchen")]
+    [FMODUnity.EventRef, SerializeField] private string musicHome = "event:/BKM/Kitchen Music";
     
+    [Space, Header("Transit Cutscenes")]
+    [FMODUnity.EventRef, SerializeField] private string farmTruckTransit = "event:/SFX_TRUCK/Farm Truck Transit";
+
     private FMOD.Studio.EventInstance _instanceSong;
     private FMOD.Studio.EventInstance _instanceTransitionFx;
 
     public int debugLevelTest;
+   
     private void Start()
     {
+        string currentScene = SceneManager.GetActiveScene().name;
+
+        switch (currentScene)
+        {
+            case "!Main Menu":
+                debugLevelTest = 0;
+                StopMusicImmediate();
+                MenuMusic();
+                break;
+            
+            case "!Farm Level":
+                debugLevelTest = 1;
+                _instanceSong.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                _instanceSong.release();
+                FarmMusic();
+                break;
+            
+            case "!Factory Level":
+                debugLevelTest = 2;
+                _instanceSong.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                _instanceSong.release();
+                FactoryMusic();
+                break;
+            
+            case "!Kitchen Level":
+                debugLevelTest = 3;
+                _instanceSong.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                _instanceSong.release();
+                HomeMusic();
+                break;
+            
+            case "!Ending Scene":
+                debugLevelTest = 3;
+                _instanceSong.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                _instanceSong.release();
+                HomeMusic();
+                break;
+            
+            default:
+                _instanceSong.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+                _instanceSong.release();
+                break;
+        }
+
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("AudioManager");
+        if (objs.Length > 1)
+            Destroy(objs[0]);
+        
+        DontDestroyOnLoad(this.gameObject);
+        
+        /*
         if (debugLevelTest == 0)
         {
             FarmMusic();
@@ -35,27 +98,27 @@ public class BKM : MonoBehaviour
             FarmMusic();
             _instanceSong.setVolume(0.5f);
         }
-    }
-
-    public void StopMusic()
-    {
-        _instanceSong.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        */
     }
     
-    //Debugging if functions work properly
-    private void Update()
+    
+
+    public void StopMusicImmediate()
     {
-    /*
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TruckDriving();
-        }
-        
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            TruckTransition();
-        }
-        */
+        _instanceSong.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        _instanceSong.release();
+    }
+
+    public void StopMusicFade()
+    {
+        _instanceSong.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        _instanceSong.release();
+    }
+
+    private void MenuMusic()
+    {
+        _instanceSong = FMODUnity.RuntimeManager.CreateInstance(musicMenu);
+        _instanceSong.start();
     }
 
     #region Farm
@@ -78,6 +141,16 @@ public class BKM : MonoBehaviour
 
     #endregion
 
+    #region Home
+
+    private void HomeMusic() 
+    { 
+        _instanceSong = FMODUnity.RuntimeManager.CreateInstance(musicHome);
+        _instanceSong.start();
+    }
+
+    #endregion
+    
     #region Transit
 
     public void TruckDriving()
@@ -96,11 +169,6 @@ public class BKM : MonoBehaviour
 
     #endregion
 
-    public void MainMenuTransition()
-    {
-        _instanceSong.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-    }
-    
     private IEnumerator WaitForEnd(float length)
     {
         yield return new WaitForSeconds(length);
