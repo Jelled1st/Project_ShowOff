217e0a61d80014bb639806bc31f717da389e9537
Gabe <glebzex@mail.ua>
2020-06-14 19:27:14 +0300

    [ADD] Factory timer

---
+++ Assets/Scripts/Factory/FactoryTimer.cs
@@ -0,0 +1,36 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Factory
+{
+    public class FactoryTimer : MonoBehaviour
+    {
+        public event Action TimeEnded = delegate { };
+
+        [SerializeField]
+        private float _roundTime;
+
+        private FactoryUiManager _factoryUiManager;
+        private Sequence _timerSequence;
+
+        private void Awake()
+        {
+            _factoryUiManager = FindObjectOfType<FactoryUiManager>();
+        }
+
+        public void StartTimer()
+        {
+            if (_timerSequence != null)
+            {
+                _timerSequence.Kill();
+                _timerSequence = null;
+            }
+
+            _timerSequence = DOTween.Sequence()
+                .AppendInterval(_roundTime)
+                .OnUpdate(delegate { _factoryUiManager.SetTimer(_timerSequence.Elapsed()); })
+                .OnComplete(delegate { TimeEnded(); });
+        }
+    }
+}
