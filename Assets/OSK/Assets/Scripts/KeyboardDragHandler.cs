e484a53bd4fa795473fa3aa497ec4c95e20e2b17
Gabe <glebzex@mail.ua>
2020-06-16 13:25:19 +0300

    [ADD] OnScreenKeyboard

---
+++ Assets/OSK/Assets/Scripts/KeyboardDragHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class KeyboardDragHandler : MonoBehaviour, IDragHandler
+{
+    #region IDragHandler implementation
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    #endregion
+}
---
+++ Assets/OSK/Assets/Scripts/KeyboardScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardScript : MonoBehaviour
+{
+    public static event Action<char> KeyPressed = delegate { };
+    public static event Action BackspacePressed = delegate { };
+
+    public InputField TextField;
+    public GameObject RusLayoutSml, RusLayoutBig, EngLayoutSml, EngLayoutBig, SymbLayout;
+
+    public void alphabetFunction(string alphabet)
+    {
+        // TextField.text = TextField.text + alphabet;
+        KeyPressed(alphabet[0]);
+    }
+
+    public void BackSpace()
+    {
+        // if (TextField.text.Length > 0) TextField.text = TextField.text.Remove(TextField.text.Length - 1);
+        BackspacePressed();
+    }
+
+    public void CloseAllLayouts()
+    {
+        RusLayoutSml.SetActive(false);
+        RusLayoutBig.SetActive(false);
+        EngLayoutSml.SetActive(false);
+        EngLayoutBig.SetActive(false);
+        SymbLayout.SetActive(false);
+    }
+
+    public void ShowLayout(GameObject SetLayout)
+    {
+        CloseAllLayouts();
+        SetLayout.SetActive(true);
+    }
+}
