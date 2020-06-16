6a5e9dc1c8ae02f7302fbc9445a1a0109e098e89
Gabe <glebzex@mail.ua>
2020-06-17 01:27:39 +0300

    [ADD] Method to check if is a bad word

--- Assets/Scripts/Scores/BadWordsList.cs
+++ Assets/Scripts/Scores/BadWordsList.cs
@@ -1,10 +1,31 @@
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class BadWordsList: ScriptableObject
+public class BadWordsList : ScriptableObject
 {
+    private const string BadWordsResource = "Bad Words";
+
     [SerializeField]
     private string[] _badWords;
-    
-    public string[] BadWords => _badWords;
+
+    private static BadWordsList Instance
+    {
+        get
+        {
+            var badWordsList = Resources.Load<BadWordsList>(BadWordsResource);
+            if (badWordsList == null)
+            {
+                Debug.LogError(
+                    $"{nameof(BadWordsList)} can't find the resource file named {BadWordsResource} of type {nameof(BadWordsList)}");
+            }
+
+            return badWordsList;
+        }
+    }
+
+    public static bool IsBadWord(string word)
+    {
+        return Instance._badWords.Contains(word.ToLower());
+    }
 }
