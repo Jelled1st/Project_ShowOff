3bc67b3b32b3f04ed65bdc87eacf799f3d221582
Chloe <chloe.blaeker@gmail.com>
2020-06-19 16:21:31 +0200

    [ADD] Scarecrow easter egg

---
+++ Assets/Scripts/EasterEggController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEggController : MonoBehaviour
+{
+    [SerializeField] private int PlayCount;
+    private int _playCount;
+    private Animator _anim;
+
+    void Start()
+    {
+        _anim = gameObject.GetComponent<Animator>();
+        _playCount = PlayCount;
+    }
+
+    private void OnMouseDown()
+    {
+        if(_playCount > 0)
+        {
+            _anim.SetTrigger("isPreAnim");
+        }
+        if(_playCount <= 0)
+        {
+            _anim.SetTrigger("isFinalAnim");
+            _playCount = PlayCount +1;
+        }
+        _playCount--;
+    }
+}
