42d65a1b367e9fa6208a458a7f7ab08cc51871f0
Chloe <chloe.blaeker@gmail.com>
2020-06-19 16:53:42 +0200

    [ADD] Barrel easter egg

---
+++ Assets/Scripts/Easter Eggs/EasterEgg_barrels.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEgg_barrels : MonoBehaviour
+{
+    [SerializeField] private int PlayCount;
+    private Rigidbody _rigidBody;
+    private int _playCount;
+
+    void Start()
+    {
+        _playCount = PlayCount;
+        _rigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown()
+    {
+        if(_playCount > 0)
+        {
+            _rigidBody.AddRelativeTorque(0, -200, 0);
+        }
+        if(_playCount == 0)
+        {
+            _rigidBody.AddRelativeForce(-300, 0, 0);
+        }
+        _playCount--;
+    }
+}
--- Assets/Scripts/Easter Eggs/EasterEgg_scarecrow.cs
+++ Assets/Scripts/Easter Eggs/EasterEgg_scarecrow.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EasterEggController : MonoBehaviour
+public class EasterEgg_scarecrow : MonoBehaviour
 {
     [SerializeField] private int PlayCount;
     private int _playCount;
