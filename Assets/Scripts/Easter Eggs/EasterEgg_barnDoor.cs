ad6f2e83c19b13167652e0e5b9fce2bfa7763021
Chloe <chloe.blaeker@gmail.com>
2020-06-19 18:42:50 +0200

    [ADD] Barn door easter egg

---
+++ Assets/Scripts/Easter Eggs/EasterEgg_barnDoor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEgg_barnDoor : MonoBehaviour
+{
+    private Animator _anim;
+    void Start()
+    {
+        _anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (_anim.GetBool("isClosed"))
+        {
+            _anim.SetBool("isClosed", false);
+        }
+        else
+        {
+            _anim.SetBool("isClosed", true);
+        }
+    }
+}
