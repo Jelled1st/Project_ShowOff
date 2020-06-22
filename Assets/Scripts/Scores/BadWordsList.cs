03104aa65f4b06065c4aac5412e97a847c921886
Gabe <glebzex@mail.ua>
2020-06-22 15:48:59 +0300

    [ADD] Custom bad words

--- Assets/Scripts/Scores/BadWordsList.cs
+++ Assets/Scripts/Scores/BadWordsList.cs
@@ -1,5 +1,9 @@
+using System;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.Networking;
 
 [CreateAssetMenu]
 public class BadWordsList : ScriptableObject
@@ -8,6 +12,8 @@ public class BadWordsList : ScriptableObject
 
     [SerializeField]
     private string[] _badWords;
+
+    private string[] _customBadWords;
 
     private static BadWordsList Instance
     {
@@ -24,8 +30,34 @@ public class BadWordsList : ScriptableObject
         }
     }
 
+    [RuntimeInitializeOnLoadMethod]
+    private static void LoadCustomBadWords()
+    {
+        try
+        {
+            var path = Path.Combine(Application.streamingAssetsPath, "CustomBadWords.txt");
+            var customWords = UnityWebRequest.Get(path);
+            customWords.SendWebRequest();
+
+            var i = 0;
+            while (!customWords.isDone && i++ < 1000)
+            {
+                Thread.Sleep(1);
+            }
+
+            Instance._customBadWords = customWords.downloadHandler.text.Split('\n')
+                .Where(w => w != "\n" && w != "\r")
+                .Select(w => w.ToLower().Replace("\r", "").Replace("\n", "")).ToArray();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+        }
+    }
+
     public static bool IsBadWord(string word)
     {
-        return Instance._badWords.Contains(word.ToLower());
+        return Instance._badWords.Contains(word.ToLower()) ||
+               (Instance._customBadWords != null && Instance._customBadWords.Contains(word.ToLower()));
     }
 }
