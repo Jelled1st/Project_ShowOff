2d9910ccca50dbe6f91e12211c478e3d7ca7404d
Chloe <chloe.blaeker@gmail.com>
2020-06-23 11:43:48 +0200

    [FIX] Fixed scarecrow easter egg breaking on click spam

--- Assets/Scripts/Easter Eggs/EasterEgg_scarecrow.cs
+++ Assets/Scripts/Easter Eggs/EasterEgg_scarecrow.cs
@@ -5,24 +5,42 @@ public class EasterEgg_scarecrow : MonoBehaviour
     [SerializeField] private int PlayCount;
     private int _playCount;
     private Animator _anim;
+    private bool _isPlaying = false;
 
     void Start()
     {
         _anim = gameObject.GetComponent<Animator>();
         _playCount = PlayCount;
     }
-
     private void OnMouseDown()
     {
-        if(_playCount > 0)
+        if(_playCount > 0 && _isPlaying == false)
         {
             _anim.SetTrigger("isPreAnim");
+            print("playing pre");
         }
-        if(_playCount <= 0)
+        if(_playCount <= 0 && _isPlaying == false)
         {
             _anim.SetTrigger("isFinalAnim");
+            print("playing final");
             _playCount = PlayCount +1;
         }
-        _playCount--;
+        if (!_isPlaying)
+        {
+            _playCount--;
+        }
+        print(_playCount);
+    }
+
+    private void animationListener(string message)
+    {
+        if(message == "start")
+        {
+            _isPlaying = true;
+        }
+        if(message == "end")
+        {
+            _isPlaying = false;
+        }
     }
 }
