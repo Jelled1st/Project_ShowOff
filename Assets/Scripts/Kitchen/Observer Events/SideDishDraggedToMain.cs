69292ea3e9f79176e589b7cefd5215468d63ade7
Jelle Vrieze <jelle.vrieze@gmail.com>
2020-06-23 20:10:09 +0200

    [ADD] Quest texts striek through events

---
+++ Assets/Scripts/Kitchen/Observer Events/SideDishDraggedToMain.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideDishDraggedToMain : AObserverEvent
+{
+    public readonly Dish mainDish;
+    public readonly Dish sideDish;
+
+    public SideDishDraggedToMain(Dish dish, Dish sideDish) : base(dish)
+    {
+        this.mainDish = dish;
+        this.sideDish = sideDish;
+    }
+}
--- Assets/Scripts/Kitchen/Tutorials/BurgerTutorial.cs
+++ Assets/Scripts/Kitchen/Tutorials/BurgerTutorial.cs
@@ -5,12 +5,12 @@ public class BurgerTutorial : KitchenSubTutorial
 {
     [SerializeField] TextMeshProUGUI _pattyToBurger;
     [SerializeField] TextMeshProUGUI _finishedIngredientToBurger;
+    [SerializeField] Dish _burgerSideDish;
 
     // Start is called before the first frame update
     void Start()
     {
         _onBakingDone.AddListener(delegate { StrikeThroughText(_pattyToBurger); });
-        _onIngredientToDish.AddListener(delegate { StrikeThroughText(_finishedIngredientToBurger); });
     }
 
     // Update is called once per frame
@@ -22,5 +22,14 @@ public class BurgerTutorial : KitchenSubTutorial
     public override void IngredientDone(IIngredient ingredient)
     {
         
+    }
+
+    public override void DishComplete(Dish dish)
+    {
+        base.DishComplete(dish);
+        if(dish == _burgerSideDish)
+        {
+            _finishedIngredientToBurger.fontStyle = FontStyles.Strikethrough;
+        }
     }
 }
