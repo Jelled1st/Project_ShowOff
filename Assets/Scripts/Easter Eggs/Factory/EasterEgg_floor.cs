5136bc250335d2975297cdcfd68db656c9e3d980
Chloe <chloe.blaeker@gmail.com>
2020-06-23 17:08:03 +0200

    [ADD] Color Easter egg to factory

---
+++ Assets/Scripts/Easter Eggs/Factory/EasterEgg_floor.cs
@@ -0,0 +1,52 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEgg_floor : MonoBehaviour
+{
+    [SerializeField] private Material _mat;
+
+    private Color _colorBase;
+    [SerializeField] private Color _color1;
+    [SerializeField] private Color _color2;
+    [SerializeField] private Color _color3;
+    private int _colorCounter = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _colorBase = _mat.GetColor("_BaseColor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnMouseDown()
+    {
+        if(_colorCounter == 1)
+        {
+            _mat.DOColor(_colorBase, "_BaseColor", 1f);
+        }
+        if(_colorCounter == 2)
+        {
+            _mat.DOColor(_color1, "_BaseColor", 1f);
+        }
+        if(_colorCounter == 3){
+            _mat.DOColor(_color2, "_BaseColor", 1f);
+        }
+        if(_colorCounter == 4)
+        {
+            _mat.DOColor(_color3, "_BaseColor", 1f);
+            _colorCounter = 0;
+        }
+        _colorCounter++;
+    }
+    void OnDestroy()
+    {
+        _mat.SetColor("_BaseColor", _colorBase);
+    }
+}
